49705a4b28064133b4330518603b99e0a1b29799
DenisBonev <denis__bonev@abv.bg>
2021-04-07 15:38:01 +0300

    Home page

---
+++ src/main/java/bg/softuni/hotelagency/web/HotelRestController.java
@@ -0,0 +1,44 @@
+package bg.softuni.hotelagency.web;
+
+import bg.softuni.hotelagency.model.view.HotelCardViewModel;
+import bg.softuni.hotelagency.service.HotelService;
+import bg.softuni.hotelagency.service.PictureService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelRestController {
+
+    private final HotelService hotelService;
+    private final PictureService pictureService;
+    private final ModelMapper modelMapper;
+
+    public HotelRestController(HotelService hotelService, PictureService pictureService, ModelMapper modelMapper) {
+        this.hotelService = hotelService;
+        this.pictureService = pictureService;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<HotelCardViewModel>> getAll() {
+        List<HotelCardViewModel> hotelCardViewModels = hotelService.
+                getAllHotels().
+                stream().
+                map(h -> {
+                    HotelCardViewModel model = modelMapper.map(h, HotelCardViewModel.class);
+                    model.setMainPictureUrl(pictureService.getPicturesByHotelId(h.getId()).get(0));
+                    return model;
+                }).
+                collect(Collectors.toList());
+        System.out.println();
+        return ResponseEntity.ok().body(hotelCardViewModels);
+    }
+}
